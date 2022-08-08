7f324c2e740eb1233a53fc20e5aebce631faf68e
Höhl, Lukas <lukas.hoehl@accso.de>
2022-08-08 11:45:55 +0200

    fish
    Signed-off-by: Höhl, Lukas <lukas.hoehl@accso.de>

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,5 @@
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+set --export --prepend PATH "/Users/hoehl/.rd/bin"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
+
+starship init fish | source
