efb24e01ca2506b0f3d99a4e67e9cbe1f4bed319
Lukas Hoehl <lukas.hoehl@stackit.de>
2024-10-08 09:47:19 +0200

    blink
    Signed-off-by: Lukas Hoehl <lukas.hoehl@stackit.de>

--- dot_config/nvim/lua/plugins/disable-defaults.lua
+++ dot_config/nvim/lua/plugins/disable-defaults.lua
@@ -1,4 +1,13 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "folke/trouble.nvim", enabled = false },
+  { "hrsh7th/nvim-cmp", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  -- { "folke/noice.nvim", opts = {
+  --   lsp = {
+  --     signature = {
+  --       enabled = false,
+  --     },
+  --   },
+  -- } },
 }
