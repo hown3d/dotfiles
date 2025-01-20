35d24b62563c332f6311c9839b0a96186c530483
Lukas Hoehl <lukas.hoehl@stackit.cloud>
2025-01-20 14:25:37 +0100

    adjust kubeconfig function
    Signed-off-by: Lukas Hoehl <lukas.hoehl@stackit.cloud>

--- dot_config/fish/functions/kubeconfig.fish
+++ dot_config/fish/functions/kubeconfig.fish
@@ -1,6 +1,7 @@
 function kubeconfig -a file
-    if test -z "$file"
-        set file $(ls $HOME/.kube/configs | fzf --preview="bat $HOME/.kube/configs/{}")
+    set __file $file
+    if test -z "$__file"
+        set __file $(ls $HOME/.kube/configs | fzf --preview="bat $HOME/.kube/configs/{}")
     end
-    set --export KUBECONFIG $HOME/.kube/configs/$file
+    set --export KUBECONFIG $HOME/.kube/configs/$__file
 end
