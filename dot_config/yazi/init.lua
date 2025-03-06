3ebcd254b0e3845285ef56f9bc5ae2dbe2e37f3c
Lukas Hoehl <lukas.hoehl@stackit.cloud>
2025-03-06 10:51:53 +0100

    drop yazi
    Signed-off-by: Lukas Hoehl <lukas.hoehl@stackit.cloud>

--- dot_config/yazi/init.lua
+++
@@ -1,9 +0,0 @@
-require("searchjump"):setup({
-	opt_unmatch_fg = "#928374",
-	opt_match_str_fg = "#000000",
-	opt_match_str_bg = "#73AC3A",
-	opt_lable_fg = "#EADFC8",
-	opt_lable_bg = "#BA603D",
-	opt_only_current = false, -- only search the current window
-	opt_search_patterns = {}, -- demo:{"%.e%d+","s%d+e%d+"}
-})
